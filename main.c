0c066b333425e87edc83052b0577b4dba87c4dfc
Iain Taylor <iain.taylor.94@gmail.com>
2023-11-15 15:30:18 +0000

    completed struct jumper - initJumper() - printJumper()

--- main.c
+++ main.c
@@ -4,9 +4,21 @@
 #include <stdlib.h>
 #include <sys/errno.h>
 
+struct jumper {
+int sequence [3000];
+int length;
+};
+
+int kgMaxSequenceLength = 3000;
+
 FILE *gInputFile;
 
 void usage (char *);
+
+struct jumper initJumper (void);
+void printJumper (struct jumper);
+
+bool isJolly (struct jumper);
 
 int main(int argc, char *argv[]) {
 
@@ -22,7 +34,12 @@ int main(int argc, char *argv[]) {
   }
 
   /*--------------------------------MAIN PROGRAM START------------------------------------*/
+  // Get first jumper
+  struct jumper jumper1 = initJumper();
+  printJumper (jumper1);
+  printf ("\n");
 
+  // Determine if jumper is "Jolly"
   
 
   
@@ -38,4 +55,45 @@ int main(int argc, char *argv[]) {
 void usage (char *cmd) {
   fprintf (stderr, "usage: %s inputFileName\n", cmd);
   exit (EXIT_SUCCESS);
+}
+
+// Jumper Functions
+struct jumper initJumper (void) {
+  struct jumper temp; // { int length; int sequence [3000] }
+  
+  char input [kgMaxSequenceLength];
+  fgets (input, kgMaxSequenceLength, gInputFile);
+  for (int i = 0; i < kgMaxSequenceLength; i++) {
+    if (input[i] == '\n') {
+      input[i] = '\0';
+      break;
+    }
+  }
+  
+  temp.length = atoi(strtok(input, " ")); 
+  for (int i = 0; i < temp.length; i++) {
+    temp.sequence [i] = atoi(strtok(NULL, " "));
+  }
+
+  return temp;
+}
+
+void printJumper (struct jumper j) {
+  printf ("Jumper Length: %d\n", j.length);
+
+  printf ("Jumper Sequence: ");
+  for (int i = 0; i < j.length; i++) {
+    printf ("%d ", j.sequence[i]);
+  }
+  printf ("\n");
+}
+
+bool isJolly (struct jumper j) {
+  bool isJolly = false;
+  int jollyTable [kgMaxSequenceLength]; // 1 for jollyEntry, 0 for notJollyEntry
+  jollyTable[0] = 1;
+  for (int i = 0; i < kgMaxSequenceLength; i++) {
+    jollyTable [i] = 1;
+  }
+  return false;
 }
