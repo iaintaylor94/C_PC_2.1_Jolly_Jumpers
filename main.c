b11417ad5b1c13d9dbd9e0f905ba4a15405cfa69
Iain Taylor <iain.taylor.94@gmail.com>
2023-11-15 14:06:39 +0000

    completed file input

--- main.c
+++ main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <sys/errno.h>
 
-int main(void) {
-  printf("Hello World\n");
+FILE *gInputFile;
+
+void usage (char *);
+
+int main(int argc, char *argv[]) {
+
+  // File Initialization
+  if (argc != 2) usage (argv[0]);
+
+  if (NULL == (gInputFile = fopen(argv[1], "r"))) {
+    fprintf (stderr, "inputFile: %s: %s\n", argv[1], strerror(errno));
+    exit (EXIT_FAILURE);
+  }
+  else {
+    fprintf (stderr, "%s opened for reading.\n" , argv[1]);
+  }
+
+  /*--------------------------------MAIN PROGRAM START------------------------------------*/
+
+  
+
+  
+
+  /*--------------------------------MAIN PROGRAM END--------------------------------------*/
+
+
+  fclose (gInputFile);
+
   return 0;
+}
+
+void usage (char *cmd) {
+  fprintf (stderr, "usage: %s inputFileName\n", cmd);
+  exit (EXIT_SUCCESS);
 }
